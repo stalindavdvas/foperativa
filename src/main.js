156a073211e5f0360f0bbd9f7bf57b007aa2cfb9
Stalin Vasco <150808453+stalindavdvas@users.noreply.github.com>
2025-02-22 20:39:40 -0500

    fix: :bug: cambio ip

--- src/main.js
+++ src/main.js
@@ -6,6 +6,6 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 const app = createApp(App);
 
 // Configurar la variable global correctamente en Vue 3
-app.config.globalProperties.$apiBaseUrl = 'http://localhost:5000';
+app.config.globalProperties.$apiBaseUrl = 'http://54.88.65.255:5000';
 
 app.use(router).mount('#app');
